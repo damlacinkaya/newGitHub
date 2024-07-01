2d5be947471c46322c491070b0f0010b33c5adf1
Damla CINKAYA <zeynepdamlakesgin@gmail.com>
2024-07-01 10:35:25 +0300

    v2.1

--- src/P01.java
+++ src/P01.java
@@ -3,5 +3,6 @@ import com.sun.tools.javac.Main;
 public class P01 {
     public static void main(String[] args) {
         System.out.println("Merhaba GitHub");
+        System.out.println("Yeni version eklme");
     }
 }
---
+++ src/P02.java
@@ -0,0 +1,10 @@
+public class P02 {
+    public static void main(String[] args) {
+        System.out.println("İkinci dosya yüklendi");
+        System.out.println("Deneme versiyonu");
+
+        int a=120;
+        int b=25;
+        System.out.println("Toplam:"+(a+b));
+    }
+}
